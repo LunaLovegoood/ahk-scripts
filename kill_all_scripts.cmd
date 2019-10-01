69dbaa9e123880dcfbff99149627e8b0027fcc25
Yurii Khomiak <luna.loooony.lovegood@gmail.com>
2019-10-02 00:51:49 +0300

    upd: removed obsolete comment about awk requirement

--- kill_all_scripts.cmd
+++ kill_all_scripts.cmd
@@ -1,7 +1,5 @@
 @echo off
 
-rem awk is needed to run this command
-rem download link: http://gnuwin32.sourceforge.net/packages/gawk.htm
 set find_script_pids_command="tasklist | awk "/^AutoHotkey.exe/ {print $2}""
 
 for /f "tokens=*" %%i in ('%find_script_pids_command%') do (

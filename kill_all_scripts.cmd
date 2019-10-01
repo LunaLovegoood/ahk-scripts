5194af5b9052bd3a27458bc97191964dce42d2dd
Yurii Khomiak <luna.loooony.lovegood@gmail.com>
2019-10-01 22:24:10 +0300

    fix: added /f flag to kill_all_scripts

--- kill_all_scripts.cmd
+++ kill_all_scripts.cmd
@@ -5,6 +5,6 @@ rem download link: http://gnuwin32.sourceforge.net/packages/gawk.htm
 set find_script_pids_command="tasklist | awk "/^AutoHotkey.exe/ {print $2}""
 
 for /f "tokens=*" %%i in ('%find_script_pids_command%') do (
-    taskkill /pid %%i > NUL
+    taskkill /f /pid %%i > NUL
     echo Ended process with pid: %%i
 )
